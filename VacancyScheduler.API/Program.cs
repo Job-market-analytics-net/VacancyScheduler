c583bc42665876f2217710e20c9517215e86fd4f
agalanov <agalanoff@gmail.com>
2024-03-12 16:34:41 +0300

    Quartz jobs implements

--- VacancyScheduler.API/Program.cs
+++ VacancyScheduler.API/Program.cs
@@ -1,11 +1,35 @@
+using Quartz;
+using VacancyScheduler.API.VacancyScheduleJobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/***********Quartz.NET*********/
+IConfigurationSection quartzConfiguration = builder.Configuration.GetSection("Quartz"); // Quartz узел конфигурации
+builder.Services.AddQuartz(q =>
+{
+    var vacancyImportJobKey = new JobKey("VacancyImportJob");
+    q.AddJob<VacancyImportJob>(opts => opts.WithIdentity(vacancyImportJobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(vacancyImportJobKey)
+        .WithIdentity("VacancyImportJob-trigger")
+        .WithCronSchedule("0 * * ? * *")            //This Cron interval can be described as "run every minute" (when second is zero)
+    );
+
+    var analyticsBuilderJobKey = new JobKey("AnalyticsBuilderJob");
+    q.AddJob<AnalyticsBuilderJob>(opts => opts.WithIdentity(analyticsBuilderJobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(analyticsBuilderJobKey)
+        .WithIdentity("AnalyticsBuilderJob-trigger")
+        .WithCronSchedule("0 * * ? * *")            //This Cron interval can be described as "run every minute" (when second is zero)
+    );
+});
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
 var app = builder.Build();
 
@@ -18,8 +42,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+//app.UseAuthorization();
 
 app.Run();
---
+++ VacancyScheduler.API/VacancyScheduleJobs/VacancyImportJob.cs
@@ -0,0 +1,15 @@
+﻿using Quartz;
+
+namespace VacancyScheduler.API.VacancyScheduleJobs
+{
+    public class VacancyImportJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            // Code that sends a periodic email to the user (for example)
+            // Note: This method must always return a value 
+            // This is especially important for trigger listers watching job execution 
+            return Task.FromResult(true);
+        }
+    }
+}
